23fe696a1caccb0cb56bcc21b191b82a68af1522
DuongCon <duongcon77@gmail.com>
2023-03-16 23:31:06 +0700

    first commit

---
+++ src/main/java/com/microservices/demo/MicroservicesMicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMicroservicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMicroservicesDemoApplication.class, args);
+	}
+
+}
