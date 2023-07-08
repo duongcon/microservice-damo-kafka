ba6251f8b5bc76aabba78585fbf068f528a91d35
DuongCon <duongcon77@gmail.com>
2023-07-08 22:24:06 +0700

    feat: add

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,20 @@
+package com.microservices.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
